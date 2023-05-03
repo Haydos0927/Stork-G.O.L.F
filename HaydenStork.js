42936ef5bf201edd95c1c4810b44026943b05394
Haydos0927 <132419222+Haydos0927@users.noreply.github.com>
2023-05-03 12:36:52 +1000

    Add files via upload

---
+++ HaydenStork.js
@@ -0,0 +1,9 @@
+function getAverage (a,b,c,d,e){
+    
+    var average = (a+b)/2;//local variables
+    console.log(average);
+    
+}
+
+var average = getAverageetAverage(1,6,8,10,5);//global variable
+alert("The average is "+ average)
